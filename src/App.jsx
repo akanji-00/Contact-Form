a39179d59f1a93f9b14400c1d6ccc8994740f643
Mohammed Adetunji <theadetunji001@gmail.com>
2026-05-05 10:46:38 +0100

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,217 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import successIcon from "./assets/icon-success-check.svg";
 
 function App() {
+  const initialState = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    queryType: "",
+    message: "",
+    consent: false,
+  };
+
+  const [formData, setFormData] = useState(initialState);
+  const [formErrors, setFormErrors] = useState({});
+  const [submittedForm, setSubmittedForm] = useState(null);
+
+  const validateForm = () => {
+    const newErrors = {};
+
+    if (!formData.firstName.trim()) {
+      newErrors.firstName = "This field is required";
+    }
+    if (!formData.lastName.trim()) {
+      newErrors.lastName = "This field is required";
+    }
+    if (!formData.email.trim()) {
+      newErrors.email = "This field is required";
+    } else if (!/^\S+@\S+\.\S+$/g.test(formData.email)) {
+      newErrors.email = "Please enter a valid email address";
+    }
+    if (!formData.queryType) {
+      newErrors.queryType = "Please select a query type";
+    }
+
+    if (!formData.message.trim()) {
+      newErrors.message = "This field is required";
+    }
+    if (!formData.consent) {
+      newErrors.consent =
+        "To submit this form, please consent to being contacted";
+    }
+
+    return newErrors;
+  };
+
+  function handleChange(e) {
+    const { name, value, type, checked } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const validationErrors = validateForm();
+
+    if (Object.keys(validationErrors).length > 0) {
+      setFormErrors(validationErrors);
+      return;
+    }
+
+    setFormData(initialState);
+    setFormErrors({});
+    setSubmittedForm(formData);
+  };
+
+  useEffect(() => {
+    if (!submittedForm) return;
+
+    const timer = setTimeout(() => {
+      setSubmittedForm(null);
+
+      return () => clearTimeout(timer);
+    }, 3000);
+  }, [submittedForm]);
+
   return (
     <main className="ff-karla">
-      <div className="container mg-2">
+      <div className="container form-wrapper mg-2">
         <h1 className="text-h font-lg mg-2">Contact Us</h1>
 
-        <form className="flex flex-col gap-2">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-2">
           <div className="flex-field gap-2">
-            <label htmlFor="first-name" className="flex flex-col py-1">
-              <span className="query-field">First Name *</span>
-              <input type="text" id="first-name" />
-            </label>
-            <label htmlFor="last-name" className="flex flex-col py-1">
-              <span className="query-field">Last Name *</span>
-              <input type="text" id="last-name" />
-            </label>
+            <div>
+              <label htmlFor="first-name" className="flex flex-col py-1">
+                <span className="query-field">First Name *</span>
+                <input
+                  type="text"
+                  value={formData.firstName}
+                  onChange={handleChange}
+                  name="firstName"
+                  id="first-name"
+                  className={formErrors.firstName ? "input error" : "input"}
+                />
+              </label>
+              {formErrors.firstName && (
+                <p className="error">{formErrors.firstName}</p>
+              )}
+            </div>
+            <div>
+              <label htmlFor="last-name" className="flex flex-col py-1">
+                <span className="query-field">Last Name *</span>
+                <input
+                  type="text"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                  name="lastName"
+                  id="last-name"
+                  className={formErrors.lastName ? "input error" : "input"}
+                />
+              </label>
+              {formErrors.lastName && (
+                <p className="error">{formErrors.lastName}</p>
+              )}
+            </div>
           </div>
 
-          <label htmlFor="email" className="flex flex-col py-1">
-            <span className="query-field">Email Address *</span>
-            <input type="email" id="email" />
-          </label>
+          <div>
+            <label htmlFor="email" className="flex flex-col py-1">
+              <span className="query-field">Email Address *</span>
+              <input
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                type="email"
+                id="email"
+                className={formErrors.email ? "input error" : "input"}
+              />
+            </label>
+            {formErrors.email && <p className="error">{formErrors.email}</p>}
+          </div>
 
           <div className="py-1">
             <span className="query-field">Query Type *</span>
             <div className="flex-field gap-2 query-labels">
-              <label htmlFor="general">
-                <input type="radio" name="query-type" value="general" />
+              <label htmlFor="general" className="radio-card">
+                <input
+                  type="radio"
+                  onChange={handleChange}
+                  name="queryType"
+                  checked={formData.queryType === "general"}
+                  id="general"
+                  value="general"
+                />
                 General Enquiry
               </label>
-              <label htmlFor="support">
-                <input type="radio" name="query-type" value="support" />
+              <label htmlFor="support" className="radio-card">
+                <input
+                  type="radio"
+                  checked={formData.queryType === "support"}
+                  id="support"
+                  value="support"
+                  onChange={handleChange}
+                  name="queryType"
+                />
                 Support Request
               </label>
             </div>
+            {formErrors.queryType && (
+              <p className="error">{formErrors.queryType}</p>
+            )}
           </div>
 
-          <label htmlFor="message" className="flex flex-col py-1">
-            <span className="query-field">Message *</span>
-            <textarea id="message" rows="5"></textarea>
-          </label>
+          <div>
+            <label htmlFor="message" className="flex flex-col py-1">
+              <span className="query-field">Message *</span>
+              <textarea
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                id="message"
+                rows="5"
+                className={formErrors.message ? "input error" : "input"}
+              ></textarea>
+            </label>
+            {formErrors.message && (
+              <p className="error">{formErrors.message}</p>
+            )}
+          </div>
 
-          <label htmlFor="consent" className="consent">
-            <input type="checkbox" id="consent" />
-            <span className="query-field">
-              I consent to being contacted by the team *
-            </span>
-          </label>
+          <div>
+            <label htmlFor="consent" className="consent">
+              <input
+                type="checkbox"
+                name="consent"
+                onChange={handleChange}
+                id="consent"
+                checked={formData.consent}
+              />
+              <span className="query-field">
+                I consent to being contacted by the team *
+              </span>
+            </label>
+            {formErrors.consent && (
+              <p className="error">{formErrors.consent}</p>
+            )}
+          </div>
 
           <button type="submit">Submit</button>
         </form>
-      </div>
 
-      {/* !/^\S+@\S+\.\S+$/ regex code */}
-      {/* This field is required This field is required Email Address Please enter a
-      valid email address This field is required Query Type General Enquiry
-      Support Request Please select a query type Message This field is required
-      I consent to being contacted by the team To submit this form, please
-      consent to being contacted Submit Message Sent! Thanks for completing the
-      form. We'll be in touch soon! */}
+        {submittedForm && (
+          <div className="submitted-data-card">
+            <div className="card-header">
+              <img src={successIcon} alt="Success Icon" />
+              <p>Message Sent!</p>
+            </div>
+            <p>Thanks for completing the form. We'll be in touch soon!</p>
+          </div>
+        )}
+      </div>
     </main>
   );
 }
